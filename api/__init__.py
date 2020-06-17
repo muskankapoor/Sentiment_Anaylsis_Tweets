a4156d76e1971a1c71bfb43c165b67e24403fba5
Yuna Kim <dev@yunasky.com>
2020-06-16 22:41:45 -0400

    Add CORS header for api endpoint

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from sentiment import RandomSentimenter
 from tweets import RandomTweeter
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/v1/sentiment_analyses/<int:tweet_id>')
 def tweet_analysis(tweet_id):
