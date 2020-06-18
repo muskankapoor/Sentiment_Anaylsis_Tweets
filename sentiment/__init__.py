f9f2e0b385d0a5843d5477751e9145fd6cf9bc86
Joseph <Joseph.Ruocco21@myhunter.cuny.edu>
2020-06-18 19:04:28 -0400

    new branch class stuff

--- sentiment/__init__.py
+++ sentiment/__init__.py
@@ -1,72 +1,7 @@
-import json
-from ibm_watson import ToneAnalyzerV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
-#authenticator = IAMAuthenticator('{apikey}')
-#tone_analyzer = ToneAnalyzerV3(
-#    version='{version}',
-#    authenticator=authenticator
-#)
-# tone_analyzer.set_service_url('{url}')
+from abc import ABC, abstractmethod
 
 class Sentimenter(ABC):
     @abstractmethod
     def analyze_text(self, text):
         pass
-
-# Authentication via external config like VCAP_SERVICES
-tone_analyzer = ToneAnalyzerV3(version='2020-06-17')
-tone_analyzer.set_service_url('https://gateway.watsonplatform.net/tone-analyzer/api')
-
-text = "this is a sample tweet #analyze" 
-
-def analyze_text(self, text):
-    tone_analysis = tone_analyzer.tone({'text': text}, content_type='application/json' ).get_result()
-    return tone_analysis
-
-# print(json.dumps(tone_analysis, indent=2))
-
-#
-#from abc import ABC, abstractmethod
-#import random
-#
-#
-
-#
-#class RandomSentimenter(Sentimenter):
-#    def __init__(self, seed=None):
-#        if seed:
-#            random.seed(seed)
-#
-#    def analyze_text(self, text):
-#        return self.generate_random_analysis()
-#
-#
-#    def generate_random_analysis(self):
-#        return {
-#            'document_tone': {
-#                'tones': self.generate_random_tones(random.randint(1, 7)),
-#            },
-#        }
-#
-#    def generate_random_tones(self, n):
-#        '''
-#        Generate n random tone scores, without repeating tones.
-#        '''
-#
-#        tone_ids = ['anger', 'fear', 'joy', 'sadness',
-#                    'analytical', 'confident', 'tentative']
-#        n = min(n, len(tone_ids))
-#        random.shuffle(tone_ids)
-#
-#        tones = []
-#        for i in range(n):
-#            tone = {
-#                'score': random.random(),
-#                'tone_id': tone_ids.pop(),
-#            }
-#            tones.append(tone)
-#
-#        return tones
-#
 
---
+++ sentiment/analyze.py
@@ -0,0 +1,58 @@
+import json
+from ibm_watson import ToneAnalyzerV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+from sentiment import Sentimenter
+
+#authenticator = IAMAuthenticator('{apikey}')
+#tone_analyzer = ToneAnalyzerV3(
+#    version='{version}',
+#    authenticator=authenticator
+#)
+# tone_analyzer.set_service_url('{url}')
+
+# Authentication via external config like VCAP_SERVICES
+tone_analyzer = ToneAnalyzerV3(version='2020-06-17')
+tone_analyzer.set_service_url('https://gateway.watsonplatform.net/tone-analyzer/api')
+
+# text = "this is a sample tweet #analyze" 
+
+# print(json.dumps(tone_analysis, indent=2))
+
+class WatsonSentimenter(Sentimenter):
+    def __init__(self, seed=None):
+        if seed:
+            random.seed(seed)
+            
+    def analyze_text(self, text):
+        tone_analysis = tone_analyzer.tone({'text': text}, content_type='application/json' ).get_result()
+        return tone_analysis
+
+#   def generate_random_analysis(self):
+#       return {
+#           'document_tone': {
+#               'tones': self.generate_random_tones(random.randint(1, 7)),
+#           },
+#       }
+#
+#   def generate_random_tones(self, n):
+#       '''
+#       Generate n random tone scores, without repeating tones.
+#       '''
+#
+#       tone_ids = ['anger', 'fear', 'joy', 'sadness',
+#                   'analytical', 'confident', 'tentative']
+#       n = min(n, len(tone_ids))
+#       random.shuffle(tone_ids)
+#
+#       tones = []
+#       for i in range(n):
+#           tone = {
+#               'score': random.random(),
+#               'tone_id': tone_ids.pop(),
+#           }
+#           tones.append(tone)
+#
+#       return tones
+#
+#
