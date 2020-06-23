f39940103345513e124325efc4cd7202ed63affb
josephruocco <40696446+josephruocco@users.noreply.github.com>
2020-06-23 18:23:03 -0400

    Add Watson API for sentiment analysis

---
+++ sentiment/analyze.py
@@ -0,0 +1,13 @@
+import json
+from ibm_watson import ToneAnalyzerV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+from sentiment import Sentimenter
+
+# Authentication via external config like VCAP_SERVICES
+tone_analyzer = ToneAnalyzerV3(version='2020-06-17')
+
+class WatsonSentimenter(Sentimenter):        
+    def analyze_text(self, text):
+        tone_analysis = tone_analyzer.tone({'text': text}, content_type='application/json' ).get_result()
+        return tone_analysis
---
+++ tests/sentiment/test_analyze.py
@@ -0,0 +1,81 @@
+import os
+import unittest
+
+from sentiment.analyze import WatsonSentimenter
+
+
+text = 'Team, I know that times are tough! Product '\
+    'sales have been disappointing for the past three '\
+    'quarters. We have a competitive product, but we '\
+    'need to do a better job of selling it!'
+
+output = {
+  "document_tone": {
+    "tones": [
+      {
+        "score": 0.6165,
+        "tone_id": "sadness",
+        "tone_name": "Sadness"
+      },
+      {
+        "score": 0.829888,
+        "tone_id": "analytical",
+        "tone_name": "Analytical"
+      }
+    ]
+  },
+  "sentences_tone": [
+    {
+      "sentence_id": 0,
+      "text": "Team, I know that times are tough!",
+      "tones": [
+        {
+          "score": 0.801827,
+          "tone_id": "analytical",
+          "tone_name": "Analytical"
+        }
+      ]
+    },
+    {
+      "sentence_id": 1,
+      "text": "Product sales have been disappointing for the past three quarters.",
+      "tones": [
+        {
+          "score": 0.771241,
+          "tone_id": "sadness",
+          "tone_name": "Sadness"
+        },
+        {
+          "score": 0.687768,
+          "tone_id": "analytical",
+          "tone_name": "Analytical"
+        }
+      ]
+    },
+    {
+      "sentence_id": 2,
+      "text": "We have a competitive product, but we need to do a better job of selling it!",
+      "tones": [
+        {
+          "score": 0.506763,
+          "tone_id": "analytical",
+          "tone_name": "Analytical"
+        }
+      ]
+    }
+  ]
+}
+
+class WatsonSentimenterTest(unittest.TestCase):
+    def setUp(self):
+        self.sentiment = WatsonSentimenter()
+
+    def tearDown(self):
+        pass
+
+    def test_watsonsentimenter(self):
+#        print(self.sentiment.analyze_text(text))
+        self.assertEqual(self.sentiment.analyze_text(text), output )
+
+
+        
