aba16707030bc848bdea2e74d456add470c72c6f
Muskan Kapoor <muskankapoor016@gmail.com>
2020-06-27 22:52:05 -0400

    latest update with fetch tweets code

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,171 +1,25 @@
-""" import sys, tweepy
-
-
-def TwitterAuthentication():
-    try:
-        consumer_key = 'gNVF7jHVY0ey7IFb3QXjfUuLh'
-        consumer_secret = 'gHOBoGOCvW0JLREPo49P9dgbBFv5hvQlftbExjHTziBnnOLO4e'
-        access_token = '1272309012000919552-2TNSU44SUSYQVbxKBZTdF8htxrHdG5'
-        access_secret = 'Bv47SjOXoeCts4zmnAR7dEUF6NWuAv0oi1LZZpzXLTItU'
-    except KeyError:
-        sys.stderr.write("Error")
-        sys.exit(1);
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
-    auth.set_access_token(access_token, access_secret)
-
-    return auth
-
-def TwitterClient():
-    auth = TwitterAuthentication #calls the function above
-    client = tweepy.API(auth, wait_on_reate_limit=True)
-    return client
-
-if  __name__ == '__main__':
-    user = input("Enter username: ")
-    client = TwitterClient()
-
-    for i in tweepy.Cursor(client.home_timeline, screen_name=user).items(10):
-        print(i.text) 
-
-
-        
-"""
-'''
-Gets text content for tweet IDs
-'''
-
-# standard
-#from __future__ import print_function
-import getopt
-import logging
-import os
-import sys
-# import traceback
-# third-party: `pip install tweepy`
 import tweepy
+import csv
 
-# global logger level is configured in main()
-Logger = None
 
-# Generate your own at https://apps.twitter.com/app
-CONSUMER_KEY = 'gNVF7jHVY0ey7IFb3QXjfUuLh'
-CONSUMER_SECRET =  'gHOBoGOCvW0JLREPo49P9dgbBFv5hvQlftbExjHTziBnnOLO4e'
-OAUTH_TOKEN = '1272309012000919552-2TNSU44SUSYQVbxKBZTdF8htxrHdG5'
-OAUTH_TOKEN_SECRET = 'Bv47SjOXoeCts4zmnAR7dEUF6NWuAv0oi1LZZpzXLTItU'
+# authentication tokens
+consumer_key = 'gNVF7jHVY0ey7IFb3QXjfUuLh'
+consumer_secret = 'gHOBoGOCvW0JLREPo49P9dgbBFv5hvQlftbExjHTziBnnOLO4e'
+access_token = '1272309012000919552-OqjqeaO0dtLrNhBn9jz9veJV1MmlKW'
+access_token_secret = '12OofbRTVGEHiFs4BLYih2i0n4QFUblTk50EYxAlSXQYS'
 
-# batch size depends on Twitter limit, 100 at this time
-batch_size=100
+authenticate = tweepy.OAuthHandler(consumer_key, consumer_secret)
+authenticate.set_access_token(access_token, access_token_secret)
+api_access = tweepy.API(authenticate,wait_on_rate_limit=True)
 
-def get_tweet_id(line):
-    '''
-    Extracts and returns tweet ID from a line in the input.
-    '''
-    (tagid,_timestamp,_sandyflag) = line.split('\t')
-    (_tag, _search, tweet_id) = tagid.split(':')
-    return tweet_id
+# i used the csv file to kind of provide a format
+#csv_file = open('example.csv', 'a')
+#csv_write = csv.writer(csv_file)
 
-def get_tweets_single(twapi, idfilepath):
-    '''
-    Fetches content for tweet IDs in a file one at a time,
-    which means a ton of HTTPS requests, so NOT recommended.
+for tweet in tweepy.Cursor(api_access.search,q="#KylieJenner",count=10, lang="en", since="2020-05-01").items():
+    print (tweet.created_at, tweet.text)
+    #csv_write.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+    
+      
 
-    `twapi`: Initialized, authorized API object from Tweepy
-    `idfilepath`: Path to file containing IDs
-    '''
-    # process IDs from the file
-    with open(idfilepath, 'rb') as idfile:
-        for line in idfile:
-            tweet_id = get_tweet_id(line)
-            Logger.debug('get_tweets_single: fetching tweet for ID %s', tweet_id)
-            try:
-                tweet = twapi.get_status(tweet_id)
-                print('%s,%s' % (tweet_id, tweet.text.encode('UTF-8')))
-            except tweepy.TweepError as te:
-                Logger.warn('get_tweets_single: failed to get tweet ID %s: %s', tweet_id, te.message)
-                # traceback.print_exc(file=sys.stderr)
-        # for
-    # with
-
-def get_tweet_list(twapi, idlist):
-    '''
-    Invokes bulk lookup method.
-    Raises an exception if rate limit is exceeded.
-    '''
-    # fetch as little metadata as possible
-    tweets = twapi.statuses_lookup(id_=idlist, include_entities=False, trim_user=True)
-    if len(idlist) != len(tweets):
-        Logger.warn('get_tweet_list: unexpected response size %d, expected %d', len(tweets), len(idlist))
-    for tweet in tweets:
-        print('%s,%s' % (tweet.id, tweet.text.encode('UTF-8')))
-
-def get_tweets_bulk(twapi, idfilepath):
-    '''
-    Fetches content for tweet IDs in a file using bulk request method,
-    which vastly reduces number of HTTPS requests compared to above;
-    however, it does not warn about IDs that yield no tweet.
-
-    `twapi`: Initialized, authorized API object from Tweepy
-    `idfilepath`: Path to file containing IDs
-    '''    
-    # process IDs from the file
-    tweet_ids = list()
-    with open(idfilepath, 'rb') as idfile:
-        for line in idfile:
-            tweet_id = get_tweet_id(line)
-            Logger.debug('Enqueing tweet ID %s', tweet_id)
-            tweet_ids.append(tweet_id)
-            # API limits batch size
-            if len(tweet_ids) == batch_size:
-                Logger.debug('get_tweets_bulk: fetching batch of size %d', batch_size)
-                get_tweet_list(twapi, tweet_ids)
-                tweet_ids = list()
-    # process remainder
-    if len(tweet_ids) > 0:
-        Logger.debug('get_tweets_bulk: fetching last batch of size %d', len(tweet_ids))
-        get_tweet_list(twapi, tweet_ids)
-
-def usage():
-    print('Usage: get_tweets_by_id.py [options] file')
-    print('    -s (single) makes one HTTPS request per tweet ID')
-    print('    -v (verbose) enables detailed logging')
-    sys.exit()
-
-def main(args):
-    logging.basicConfig(level=logging.WARN)
-    global Logger
-    Logger = logging.getLogger('get_tweets_by_id')
-    bulk = True
-    try:
-        opts, args = getopt.getopt(args, 'sv')
-    except getopt.GetoptError:
-        usage()
-    for opt, _optarg in opts:
-        if opt in ('-s'):
-            bulk = False
-        elif opt in ('-v'):
-            Logger.setLevel(logging.DEBUG)
-            Logger.debug("main: verbose mode on")
-        else:
-            usage()
-    if len(args) != 1:
-        usage()
-    idfile = args[0]
-    if not os.path.isfile(idfile):
-        print('Not found or not a file: %s' % idfile, sys.stderr)
-        usage()
-
-    # connect to twitter
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-    api = tweepy.API(auth)
-
-    # hydrate tweet IDs
-    if bulk:
-        get_tweets_bulk(api, idfile)
-    else:
-        get_tweets_single(api, idfile)
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+       
