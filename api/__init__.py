119576c8265e22e93e53670118d9668458028474
Yuna Kim <dev@yunasky.com>
2020-07-02 04:16:09 -0400

    Integrate twitter and plumb it together (#13)

--- api/__init__.py
+++ api/__init__.py
@@ -1,19 +1,31 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify
 from flask_cors import CORS
 
+import error
 from cache.sqlite import SqliteCache
-from sentiment import RandomSentimenter
 from sentiment.caching import CachingSentimenter
+from sentiment.watson import Watson
+from tweets.twitter import Twitter
 
 CACHE_DB_PATH = 'db/cache.db'
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+twitter = Twitter(os.environ.get('TWITTER_CONSUMER_KEY'),
+                  os.environ.get('TWITTER_CONSUMER_SECRET'))
+
 @app.route('/api/v1/sentiment_analyses/<int:tweet_id>')
 def tweet_analysis(tweet_id):
     cache = SqliteCache("sentiments", CACHE_DB_PATH)
-    random_sent = RandomSentimenter()
-    sentimenter = CachingSentimenter(cache, random_sent)
+    watson = Watson()
+    sentimenter = CachingSentimenter(cache, watson)
 
-    return sentimenter.analyze_text(str(tweet_id))
+    try:
+        text = twitter.get_tweet_text(tweet_id)
+    except error.TweetNotFoundError:
+        return jsonify(error='No tweet found with that ID.'), 404
+
+    return sentimenter.analyze_text(text)
